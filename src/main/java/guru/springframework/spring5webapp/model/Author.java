0b471dd09c01f01b3d432bd706ea5770e8a27f80
PierreSQS <nomo_messina@web.de>
2021-04-03 00:05:40 +0200

    Exercise3: fixed the issue on previous Commit (915399d4)

--- src/main/java/guru/springframework/spring5webapp/model/Author.java
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -1,28 +1,28 @@
 package guru.springframework.spring5webapp.model;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.RequiredArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Data
-@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     Long id;
-    private final String firstName;
-    private final String lastName;
+    private String firstName;
+    private String lastName;
 
     @ManyToMany(mappedBy = "authors")
     @EqualsAndHashCode.Exclude
     @ToString.Exclude
     Set<Book> books = new HashSet<>();
 
-
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
